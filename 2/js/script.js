af86db5f109eeadf2a929cef1cf5a72600fc791b
Rohan Bhansali <rohanb10@gmail.com>
2016-05-16 12:44:20 +0530

    optimized scroll/animate code

--- 2/js/script.js
+++ 2/js/script.js
@@ -79,9 +79,9 @@ $(".project-list a").click(function() {
 		}
 
 		//hide current screen
-		$(".iphone-screen").hide('slide', { direction: "left"},800);
-		$(".laptop-screen").hide('slide', { direction: "left"},800);
-		$(".project-description").hide('fade',802);
+		$(".iphone-screen").hide('slide', { direction: "left"},1000);
+		$(".laptop-screen").hide('slide', { direction: "left"},1000);
+		$(".project-description").hide('fade',1050);
 		var btn = $(this);
 
 		//remove old button
@@ -96,68 +96,53 @@ $(".project-list a").click(function() {
 			$(".iphone-screen").attr("src", "img/img-container/"+project+"-iphone.jpg");
 			$(".laptop-screen").attr("src", "img/img-container/"+project+"-laptop.jpg");
 			//show new screen
-			$(".iphone-screen").show('slide', { direction: "left"},800);
-			$(".laptop-screen").show('slide', { direction: "left"},800);
+			$(".iphone-screen").show('slide', { direction: "left"},1000);
+			$(".laptop-screen").show('slide', { direction: "left"},1000);
 			//change to new button
 			btn.css('box-shadow', 'inset 250px 0 0 0 #31302B');
 			btn.css('color', '#F3F3F3');
 			
 			//change descriptions
-			$(".project-description").show('fade',800);
-			$(".project-github").show('fade',800);
-			if(project == "sl") {
-				$(".project-description").text("I run the website for Student Life. Built using the LAMP stack with Wordpress, it recieves 100,000+ page views from 45,000+ views every month.");
-				$(".project-url").attr("href","http://studlife.com");
-				$(".project-github").attr("href","http://github.com/rohanb10/studlife");
-			}
-			if(project == "sl-housing") {
-				$(".project-description").text("A comprehensive housing guide for all official campus living options for Washington Univeristy. Check out the website and source code below.");
-				$(".project-url").attr("href","http://studlife.com/housing");
-				$(".project-github").attr("href","http://github.com/rohanb10/studlife");
-			}
-			if(project == "sl-whowon") {
-				$(".project-description").text("An end of year bracket recapping the 32 most notable events on WUSTL's campus in 2015-16. This piece was split into 5 parts, and had a staggered release on the website over the course of a week.");
-				$(".project-url").attr("href","http://studlife.com/whowon1516");
-				$(".project-github").attr("href","http://github.com/rohanb10/studlife");
-			}
+			$(".project-description").show('fade',1000);
 			if(project == "ef") {
 				$(".project-description").html("As an intern for this Mumbai based startup, I worked on building the vendor facing administrative dashboard. I am not permitted to share the code I worked on, but you can view the website below. The website was built using <a href='http://meteor.com' target='_blank'>Meteor</a>.");
 				$(".project-url").attr("href","http://eazyfriday.com");
-				$(".project-github").hide('fade',800);
+				$(".project-github").hide('fade',1000);
 			}
-			if(project == "llinder") {
-				$(".project-description").text("A simple one page responsive website I built for a job application in Fall 2015. The site features pure css parallax scrolling. Check out the website and source code below.");
-				$(".project-url").attr("href","http://rohanb10.github.io/Llinder");
-				$(".project-github").attr("href","http://github.com/rohanb10/Llinder");
+			else{
+				$(".project-github").show('fade',1000);
+				if(project == "sl") {
+					$(".project-description").text("I run the website for Student Life. Built using the LAMP stack with Wordpress, it recieves 100,000+ page views from 45,000+ views every month.");
+					$(".project-url").attr("href","http://studlife.com");
+					$(".project-github").attr("href","http://github.com/rohanb10/studlife");
+				}
+				if(project == "sl-housing") {
+					$(".project-description").text("A comprehensive housing guide for all official campus living options for Washington Univeristy. Check out the website and source code below.");
+					$(".project-url").attr("href","http://studlife.com/housing");
+					$(".project-github").attr("href","http://github.com/rohanb10/studlife");
+				}
+				if(project == "sl-whowon") {
+					$(".project-description").text("An end of year bracket recapping the 32 most notable events on WUSTL's campus in 2015-16. This piece was split into 5 parts, and had a staggered release on the website over the course of a week.");
+					$(".project-url").attr("href","http://studlife.com/whowon1516");
+					$(".project-github").attr("href","http://github.com/rohanb10/studlife");
+				}
+				if(project == "llinder") {
+					$(".project-description").text("A simple one page responsive website I built for a job application in Fall 2015. The site features pure css parallax scrolling. Check out the website and source code below.");
+					$(".project-url").attr("href","http://rohanb10.github.io/Llinder");
+					$(".project-github").attr("href","http://github.com/rohanb10/Llinder");
+				}
 			}
-		},800);
+		},1000);
 	}
 	return false;
 });
 
-$("#arrow-scroll").click(function(event) {
+//scroll to div
+function scroll(classname){
 	$('html,body').animate({
-		scrollTop: $(".panel-me").offset().top
+		scrollTop: $(classname).offset().top
 	}, 1000);
-});
-
-$(".me-btn-projects").click(function(event) {
-	$('html,body').animate({
-		scrollTop: $(".panel-projects").offset().top
-	}, 1000);
-});
-
-$(".me-btn-exp").click(function(event) {
-	$('html,body').animate({
-		scrollTop: $(".panel-exp").offset().top
-	}, 1000);
-});
-
-$(".me-btn-skills").click(function(event) {
-	$('html,body').animate({
-		scrollTop: $(".panel-skills").offset().top
-	}, 1000);
-});
+}
 
 $(".img-skill").hover(function() {
 	$(".skill-hover").html($(this).attr('alt'));
@@ -166,20 +151,21 @@ $(".img-skill").hover(function() {
 	$(".skill-hover").css('opacity', '0');
 });
 
-$(document).ready(function() {
-	setTimeout(firstPanel,1000);
+function css_resize(){
+
 	$(".panel").css('min-height', window.innerHeight+"px");
 	$(".half-panel").css('min-height', (window.innerHeight/2)+"px");
 	$(".quarter-panel").css('min-height', (window.innerHeight/4)+"px");
-	
+	$(".arrow-intro").css('top', (window.innerHeight*.85)+"px");
+}
+
+$(document).ready(function() {
+	setTimeout(firstPanel,1000);
+	(window.innerWidth > window.innerHeight_) ? css_resize() :console.log() ;
 });
 
 $(window).resize(function() {
-	if(window.innerWidth > window.innerHeight) {
-		$(".panel").css('min-height', window.innerHeight+"px");
-		$(".half-panel").css('min-height', (window.innerHeight/2)+"px");
-		$(".quarter-panel").css('min-height', (window.innerHeight/4)+"px");
-	}
+	
 });
 
 $(window).load(function() {

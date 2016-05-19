a55c3b48c7c816b0c92454d25549b91c6658a834
Rohan Bhansali <rohanb10@gmail.com>
2016-05-20 02:08:30 +0530

    Finishing touches applied, Kraudesorce added to project in place of eazyfriday, ready for push to rohan.xyz

--- 2/js/script.js
+++ 2/js/script.js
@@ -103,58 +103,62 @@ function showGoogleMaps(location) {
 }
 
 function loadWorkPlace(work,location){
-	
 	if(work=="zappos"){
 		$(".img-exp").attr('src', 'img/'+work+".jpg");
 		$(".exp-title").html("Zappos.com");
 		$(".exp-position").html("Front-End Developer Intern");
 		$(".exp-date").html("Summer 2016");
-		$(".exp-text").html("I will be working as an intern at Zappos.com in Las Vegas, NV this summer.<br><br>I will be working on the FEZ (Front End Zappos) team.<br><br>Details TBD.");
+		$(".exp-text").html("<div>I will be working as an intern at Zappos.com in Las Vegas, NV this summer.</div><div>I will be working on the FEZ (Front End Zappos) team.</div><div>Details TBD.</div>");
 	}
 	if(work=="wustl"){
 		$(".img-exp").attr('src', 'img/'+work+".png");
-		$(".exp-title").html("Washington Univeristy in St. Louis");
+		$(".exp-title").html("Washington University in St. Louis");
 		$(".exp-position").html("Teaching Assistant");
 		$(".exp-date").html("6 semesters");
-		$(".exp-text").html("<strong>CSE 131 - Computer Science 1</strong><br>This course is introductary Computer Science course taught to over 500 students every semester. It covers many fundamental programming concepts including algorithms, iteration and recursion, inheritance, object oriented programming and some simple data structures. It is taught in Java. As a TA for this course, my duties include:<br><ul><li>Holding weekly lab and studio sessions for around 40 students</li><li>Conducting weekly office hours for more individualized help</li><li>Grading of labs, studios and extensions</li><li>Proctoring and grading three exams every semester</li></ul>");
+		$(".exp-text").html("<div><strong>CSE 131 - Computer Science 1</strong></div><div>This course is introductory Computer Science course taught to over 500 students every semester. It covers many fundamental programming concepts including algorithms, iteration and recursion, inheritance, object oriented programming and some simple data structures. It is taught in Java. As a TA for this course, my duties include:</div><ul><li>Holding weekly lab and studio sessions for around 40 students</li><li>Conducting weekly office hours for more individualized help</li><li>Grading of labs, studios and extensions</li><li>Proctoring and grading three exams every semester</li></ul>");
 	}
 	if(work=="sl"){
 		$(".img-exp").attr('src', 'img/'+work+".jpg");
 		$(".exp-title").html("Student Life");
 		$(".exp-position").html("Senior Online Editor");
 		$(".exp-date").html("August 2014 - Present");
-		$(".exp-text").html("Student Life is Washington University's only independent newspaper.<br>As Senior Online Editor, my duties include:<br><ul><li>Working with the editorial staff to design attractive, responsive web pages for their articles</li><li>Performing regular maintenance of servers, backups and archives</li><li>Resolve database errors due to heavy traffic or other unforeseen circumstances</li></ul>In the past, I assisted with redesign of the website to make it responsive. You can view the site <a href='http://studlife.com' target='_blank'>here</a> and view our special online projects <a href='https://github.com/rohanb10/studlife' target='_blank'>here</a>.");
+		$(".exp-text").html("<div>Student Life is Washington University's only independent newspaper.<br>As Senior Online Editor, my duties include:</div><ul><li>Working with the editorial staff to design attractive, responsive web pages for their articles</li><li>Performing regular maintenance of servers, backups and archives</li><li>Resolve database errors due to heavy traffic or other unforeseen circumstances</li></ul><div>In the past, I assisted with redesign of the website to make it responsive. You can view the site <a href='http://studlife.com' target='_blank'>here</a> and view our special online projects <a href='https://github.com/rohanb10/studlife' target='_blank'>here</a>.</div>");
 	}
 	if(work=="ef"){
 		$(".exp-title").html("EazyFriday");
 		$(".img-exp").attr('src', 'img/'+work+".jpg");
 		$(".exp-position").html("Software Developer Intern");
 		$(".exp-date").html("Summer 2015");
-		$(".exp-text").html("EazyFriday is a Mumbai based technology startup. Its goal is to allow users to request home services like electricians and plumbers through a web service and an app.<br>As an intern, I performed these tasks:<ul><li>Developed a dynamic user authorization system for retailers and administrators</li><li>Designed and built a retail management dashboard to control outlet operations</li><li>Designed and built the interface for back-end administrative controls for retailers</li><li>Created an API to be used by the mobile apps</li></ul>You can view their website <a href='http://eazyfriday.com' target='_blank'>here</a>.");
+		$(".exp-text").html("<div>EazyFriday is a Mumbai based software startup. Its goal is to allow users to request home services like electricians and plumbers through a web service and companion apps.</div><div>As an intern, I performed these tasks:</div><ul><li>Developed a dynamic user authorization system for retailers and administrators</li><li>Designed and built a retail management dashboard to control outlet operations</li><li>Designed and built the interface for back-end administrative controls for retailers</li><li>Created an API to be used by the mobile apps</li></ul>You can view their website <a href='http://eazyfriday.com' target='_blank'>here</a>.");
 	}
-	$(".exp-overlay span").css({
+	$(".exp-header span").css({
 		'background-color':'rgba(49,48,43,0.7)',
-		'color': '#F3F3F3' 
+		'color': '#F3F3F3',
 	});
-	$(".exp-overlay a").css('color','#F3F3F3');
+	$(".exp-btn a").css({
+		'border': '3px solid rgba(49,48,43,0.7)',
+		'color': '#F3F3F3',
+		'background-color': 'rgba(49,48,43,0.7)'
+	});
 	$(".exp-body").show('fade',1000);
 	exp_loaded = true;
-		//call to display correct google maps background
-		showGoogleMaps(location);
-		$("#googlemaps").css('height', parseInt($(".panel-exp").css('height'),10)+15+"px");
-		$("#bg-filter").css('height', parseInt($(".panel-exp").css('height'),10)+16+"px");
-		current_work = work;
+	//call to display correct google maps background
+	showGoogleMaps(location);
+	$("#googlemaps").css('height', parseInt($(".panel-exp").css('height'),10)+15+"px");
+	$("#bg-filter").css('height', parseInt($(".panel-exp").css('height'),10)+16+"px");
+	current_work = work;
 
-	}
+}
 
-//click experience button to show selected workplace
+//experience panel controller
 $(".exp-btn").click(function(event) {
-	//load experience description
 	var work = $(this).data('work');
 	if(work!=current_work){
 		var location = [$(this).data('lat'),$(this).data('long')];
+		//preload image for smooth transition
+		(new Image()).src = "img/"+work+".jpg";
 		if(exp_loaded){
-			$(".exp-body").hide('fade',1000);
+			$(".exp-body, #googlemaps").hide('fade',1000);
 			setTimeout(function(){
 				loadWorkPlace(work,location);
 			},1000)
@@ -165,7 +169,7 @@ $(".exp-btn").click(function(event) {
 	}
 });
 
-//project screen control
+//project panel controller
 $(".project-list a").click(function() {
 	var project = $(this).data("project");
 	//only if selected project is different
@@ -198,39 +202,38 @@ $(".project-list a").click(function() {
 			btn.addClass('active')
 			
 			//change descriptions / buttons
-			$(".project-description").show('fade',1000);
-			if(project == "ef") {
-				$(".project-description").html("As an intern for this Mumbai based startup, I worked on building the vendor facing administrative dashboard. You can view the website below. The website was built using <a href='http://meteor.com' target='_blank'>Meteor</a>.");
+			$(".project-description, .project-url").show('fade',1000);
+
+			if(project == "ks") {
+				$(".project-description").html("An app that estimates the number of people at dining halls on WUSTL's campus at any given time. Data is gathered from wifi routers across campus and an iPhone app. This project is still under active development");
 				$(".project-url").attr("href","http://eazyfriday.com");
-				$(".project-github").hide('fade',1000);
+				$(".project-github").attr("href","https://github.com/yashdalal/kraudesorce");
+				$(".project-url").hide('fade',1000);
 			}
-			else{
-				$(".project-github").show('fade',1000);
-				if(project == "sl") {
-					$(".project-description").text("I run the website for Student Life. Built using the LAMP stack with Wordpress, it recieves 100,000+ page views from 45,000+ views every month.");
-					$(".project-url").attr("href","http://studlife.com");
-					$(".project-github").attr("href","http://github.com/rohanb10/studlife");
-				}
-				if(project == "sl-housing") {
-					$(".project-description").text("A comprehensive housing guide for all official campus living options for Washington Univeristy. Check out the website and source code below.");
-					$(".project-url").attr("href","http://studlife.com/housing");
-					$(".project-github").attr("href","http://github.com/rohanb10/studlife");
-				}
-				if(project == "sl-whowon") {
-					$(".project-description").text("An end of year bracket recapping the 32 most notable events on WUSTL's campus in 2015-16. This piece was split into 5 parts, and had a staggered release on the website over the course of a week.");
-					$(".project-url").attr("href","http://studlife.com/whowon1516");
-					$(".project-github").attr("href","http://github.com/rohanb10/studlife");
-				}
-				if(project == "llinder") {
-					$(".project-description").text("A simple one page responsive website I built for a job application in Fall 2015. The site features pure css parallax scrolling. Check out the website or view the Github for more information.");
-					$(".project-url").attr("href","http://rohanb10.github.io/Llinder");
-					$(".project-github").attr("href","http://github.com/rohanb10/Llinder");
-				}
-				if(project == "personal") {
-					$(".project-description").text("I redesigned my personal website from scratch over the summer of 2016. I tried to cut down on the number of dependencies I was using. Check out the source code at my GitHub or just inspect element anywhere on the web page.");
-					$(".project-url").attr("href","http://rohan.xyz");
-					$(".project-github").attr("href","http://github.com/rohanb10/resume");
-				}
+			if(project == "sl") {
+				$(".project-description").text("I run the website for Student Life. Built using the LAMP stack with Wordpress, it recieves 100,000+ page views from 45,000+ views every month.");
+				$(".project-url").attr("href","http://studlife.com");
+				$(".project-github").attr("href","https://github.com/rohanb10/studlife");
+			}
+			if(project == "sl-housing") {
+				$(".project-description").text("A comprehensive housing guide for all official campus living options for Washington University. Check out the website and source code below.");
+				$(".project-url").attr("href","http://studlife.com/housing");
+				$(".project-github").attr("href","https://github.com/rohanb10/studlife");
+			}
+			if(project == "sl-whowon") {
+				$(".project-description").text("An end of year bracket recapping the 32 most notable events on WUSTL's campus in 2015-16. This piece was split into 5 parts, and had a staggered release on the website over the course of a week.");
+				$(".project-url").attr("href","http://studlife.com/whowon1516");
+				$(".project-github").attr("href","https://github.com/rohanb10/studlife");
+			}
+			if(project == "llinder") {
+				$(".project-description").text("A simple one page responsive website I built for a job application in Fall 2015. The site features pure css parallax scrolling. Check out the website or view the Github for more information.");
+				$(".project-url").attr("href","http://rohanb10.github.io/Llinder");
+				$(".project-github").attr("href","https://github.com/rohanb10/Llinder");
+			}
+			if(project == "personal") {
+				$(".project-description").text("I redesigned my personal website from scratch over the summer of 2016. I tried to cut down on the number of dependencies I was using. Check out the source code at my GitHub or just inspect element instead.");
+				$(".project-url").attr("href","http://rohan.xyz");
+				$(".project-github").attr("href","https://github.com/rohanb10/resume");
 			}
 		},1000);
 	}

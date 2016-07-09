8d091e4b36a78a1f007b6411f2cc7a9f28abdf01
Rohan Bhansali <rohanb10@gmail.com>
2016-07-09 01:28:33 -0700

    Begin of migration of jobs/projects to JS library

---
+++ js/lists.js
@@ -0,0 +1,79 @@
+var jobList = [
+  {
+    id:"ef",
+    name:"EazyFriday",
+    image:"img/ef.jpg",
+    position:"Software Developer Intern",
+    date:"Summer 2015",
+    text:"<div>EazyFriday is a Mumbai based software startup. Its goal is to allow users to request home services like electricians and plumbers through a web service and companion apps.</div><div>As an intern, I performed these tasks:</div><ul><li>Developed a dynamic user authorization system for retailers and administrators</li><li>Designed and built a retail management dashboard to control outlet operations</li><li>Designed and built the interface for back-end administrative controls for retailers</li><li>Contributed to an API to be used by the companion mobile apps</li></ul>You can view their website <a class='before after' href='http://eazyfriday.com' target='_blank'>here</a>."
+  },
+  {
+    id:"sl",
+    name:"Student Life",
+    image:"img/sl.jpg",
+    position:"Senior Online Editor",
+    date:"August 2014 - Present",
+    text:"<div>Student Life is Washington University's only independent newspaper.<br>As Senior Online Editor, my duties include:</div><ul><li>Working with the editorial staff to design attractive, responsive web pages for their articles</li><li>Performing regular maintenance of servers, backups and archives</li><li>Resolve database errors due to heavy traffic or other unforeseen circumstances</li></ul><div>In the past, I assisted with redesign of the website to make it responsive. You can view the site <a class='before after' href='http://studlife.com' target='_blank'>here</a> and view our special online projects <a class='before after' href='https://github.com/rohanb10/studlife' target='_blank'>here</a>.</div>"
+  },
+  {
+    id:"wustl",
+    name:"WUSTL",
+    image:"img/wustl.png",
+    position:"Teaching Assistant",
+    date:"6 semesters",
+    text:"<div><strong>CSE 131 - Computer Science 1</strong></div><div>This course is introductory Computer Science course taught to over 500 students every semester. It covers many fundamental programming concepts including algorithms, iteration and recursion, inheritance, object oriented programming and some simple data structures. It is taught in Java. As a TA for this course, my duties include:</div><ul><li>Holding weekly lab and studio sessions for around 40 students</li><li>Conducting weekly office hours for more individualized help</li><li>Grading of labs, studios and extensions</li><li>Proctoring and grading three exams every semester</li></ul>"
+  },
+  {
+    id:"zappos",
+    name:"Zappos",
+    image:"img/zappos.jpg",
+    position:"Front-End Developer Intern",
+    date:"Summer 2016",
+    text:"<div>I will be working as an intern at Zappos.com in Las Vegas, NV this summer.</div><div>I will be working on the FEZ (Front End Zappos) team.</div><div>Details TBD.</div>"
+  }
+];
+
+var projectList = [
+  {
+    id:"ks",
+    name:"Kraudesorce",
+    url:"",
+    github:"https://github.com/yashdalal/kraudesorce",
+    desription:"An app that estimates the number of people at dining halls on WUSTL's campus at any given time. Data is gathered from wifi routers across campus and an iPhone app. This project is still under active development."
+  },
+  {
+    id:"sl",
+    name:"Studlife",
+    url:"http://studlife.com",
+    github:"https://github.com/rohanb10/studlife",
+    desription:""
+  },
+  {
+    id:"sl-housing",
+    name:"Studlife Housing Guide",
+    url:"http://studlife.com/housing",
+    github:"https://github.com/rohanb10/studlife",
+    desription:"A comprehensive housing guide for all official campus living options for Washington University. Check out the website and source code below."
+  },
+  {
+    id:"sl-whowon",
+    name:"Studlife Who Won 15-16",
+    url:"http://studlife.com/whowon1516",
+    github:"https://github.com/rohanb10/studlife",
+    desription:"An end of year bracket recapping the 32 most notable events on WUSTL's campus in 2015-16. This piece was split into 5 parts, and had a staggered release on the website over the course of a week."
+  },
+  {
+    id:"llinder",
+    name:"Llinder",
+    url:"http://rohanb10.github.io/Llinder",
+    github:"https://github.com/rohanb10/llinder",
+    desription:"A simple one page responsive website I built for a job application in Fall 2015. The site features pure css parallax scrolling. Check out the website or view the Github for more information."
+  },
+  {
+    id:"personal",
+    name:"Rohan.xyz",
+    url:"http://rohan.xyz",
+    github:"https://github.com/rohanb10/resume",
+    desription:"I redesigned my personal website from scratch over the summer of 2016. Check out the source code at my GitHub or just inspect element instead. Don't forget the <a class='before after' target='_blank' href='404.html'>404 page.</a>"
+  }
+];

a117f1146fb28354f55be7523fcdc5953ce9d83b
rohan <rohanb10@gmail.com>
2019-12-19 13:56:53 +0530

    purge 19/12

--- js/script.js
+++
@@ -1,9 +0,0 @@
-function goToPage(e, pageName) {
-	// page transition
-	e.classList.add('slide-off-screen');
-	//redirect
-	setTimeout(function() {
-		window.location.href = './' + pageName + '/index.html';	
-	}, 1000);
-	
-}

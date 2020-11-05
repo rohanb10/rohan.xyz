9ec323fb51a1815c49a2c76dfc733a42b4ae770f
rohan <rohanb10@gmail.com>
2020-11-05 16:30:00 +0530

    fetch rides from strava py script

---
+++ js/fetch_latest_rides.py
@@ -0,0 +1,47 @@
+import requests
+import json
+import re
+
+quit()
+# get latest url: await firebase.storage().ref().child('refresh.json').getDownloadURL().then(f => console.log(f))
+firebase = requests.get()
+print('\nGetting data from Firebase Storage')
+
+print('\nAuthenticating Strava...')
+auth = requests.post( url = 'https://www.strava.com/oauth/token', data = firebase.json())
+strava_token = auth.json()
+print(json.dumps(strava_token, indent=4))
+
+print('\nGetting 50 latest rides from Strava...')
+activities = requests.get('https://www.strava.com/api/v3/activities' + '?access_token=' + strava_token['access_token'] + '&per_page=50' + '&page=1')
+
+def stringToJson(str): 
+	return json.loads('{' + re.sub(r'(\d{10}):', r'"\1":', str) + '}')
+
+print('\nGetting all rides previously saved.')
+with open('rides.js') as rj: 
+	ride_lists = re.findall('{¿(.+?)¿}', rj.read().replace(' ', '').replace('\t', '').replace('\n', '¿'))
+
+sf = stringToJson(ride_lists[0].replace('¿', ''))
+bom = stringToJson(ride_lists[1].replace('¿', ''))
+
+rideIDs = []
+for a in activities.json():
+	if not(str(a['id']) in bom) and not(str(a['id']) in sf): rideIDs.append(str(a['id']))
+
+print('\nFound ' + str(len(rideIDs)) + ' new rides.\n')
+if len(rideIDs) == 0: quit()
+
+newRides = {}
+for r in rideIDs[::-1]:
+	print('Fetching ride ' + r + ' from Strava')
+	ride = requests.get('https://www.strava.com/api/v3/activities/' + r + '?include_all_efforts=false' + '&access_token=' + strava_token['access_token'])
+	data = ride.json()
+	newRides[r] = str(data['map']['polyline']).replace(chr(92), chr(92) + chr(92))
+
+print('\n\nSaving rides to _new_rides.txt\n\n')
+with open('_new_rides.txt', 'w') as f:
+	for r_id, poly in newRides.items():
+		f.write(r_id + ': "' + poly + '",\n')
+
+print('Done!\n')
